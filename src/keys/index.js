fcde6f1b978594bb2d92b4e357d5380da743ad3e
Денис Магазейщиков <denismagazeyshchikov@Den.local>
2019-03-24 00:50:41 +0300

    removed keys from gitignore

---
+++ src/keys/index.js
@@ -0,0 +1,7 @@
+const URLs = {
+  production: 'http://77.37.176.224:8000',
+  mock200: 'http://www.mocky.io/v2/5c961d312f000068002396fd?mocky-delay=1000ms',
+  mock401: 'http://www.mocky.io/v2/5c9643762f00006800239731?mocky-delay=1000ms'
+}
+
+export { URLs }
