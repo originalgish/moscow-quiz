355bc17827363c2ddc4b5575730a1497b72c90b6
Денис Магазейщиков <denismagazeyshchikov@Den.local>
2019-03-23 22:31:37 +0300

    museum image

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react'
 
+import museum from './img/museum.png'
 import './index.scss'
 
 class Quiz extends Component {
   state = {}
+
+  handleAreaClick = e => {
+    e.preventDefault()
+    console.log('area')
+  }
+
   render() {
-    return <div className="quiz">quiz</div>
+    return (
+      <div className="quiz">
+        <map name="museum" className="map">
+          <area
+            alt="question"
+            shape="circle"
+            coords="150,300,50"
+            className="map__area"
+            href=""
+            onClick={this.handleAreaClick}
+          />
+        </map>
+        <img useMap="#museum" src={museum} alt="museum" className="museum" />
+
+        {/* <svg className="museum">
+          <div cx="150" cy="300" r="50" className="museum__area" />
+        </svg> */}
+      </div>
+    )
   }
 }
 
