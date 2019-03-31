192502a29df29de435b4e5ddb72eeb3118260c25
Денис Магазейщиков <denismagazeyshchikov@Den.local>
2019-03-31 17:28:01 +0300

    home page styles

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 
-import { HomeWrapper } from './styles'
+import { Wrapper, Title, City, InfoWrapper, Info, Navigation, NavLink } from './styles'
 
 class Home extends Component {
   state = {}
   render() {
-    return <HomeWrapper>home</HomeWrapper>
+    return (
+      <Wrapper>
+        <Title>Закупочный квест</Title>
+        <City>Москва</City>
+        <InfoWrapper>
+          <Info>
+            NEMO ENIM IPSAM VOLUPTATEM, QUIA VOLUPTAS SIT, ASPERNATUR AUT ODIT AUT FUGIT, SED QUIA CONSEQUUNTUR MAGNI
+            DOLORES EOS, QUI RATIONE VOLUPTATEM SEQUI NESCIUNT, NEQUE PORRO QUISQUAM EST.
+          </Info>
+          <Info>QUI DOLOREM EUM FUGIAT, QUO APRIL 4TH 20:00 VOLUPTAS NULLA PARIATUR. </Info>
+        </InfoWrapper>
+        <Navigation>
+          <NavLink to="/registration">Регистрация</NavLink>
+          <NavLink to="/login">Войти</NavLink>
+        </Navigation>
+      </Wrapper>
+    )
   }
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,3 +1,73 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-export const HomeWrapper = styled.div``
+export const Wrapper = styled.div`
+  padding: 0 10%;
+  @media screen and (max-width: 600px) {
+    padding: 0;
+  }
+`
+export const Title = styled.h1`
+  margin: 0;
+  font-family: Helvetica;
+  font-size: 3em;
+  letter-spacing: 3px;
+  text-align: center;
+  color: #ffffff;
+  text-transform: uppercase;
+  margin-top: 150px;
+  padding-bottom: 20px;
+  position: relative;
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 50%;
+    height: 3px;
+    background-color: #ffffff;
+    border-radius: 4px;
+  }
+  @media screen and (max-width: 600px) {
+    margin-top: 100px;
+  }
+`
+export const City = styled.h2`
+  margin: 0;
+  font-family: Helvetica;
+  font-size: 2.5em;
+  text-align: center;
+  color: #ffffff;
+  text-transform: uppercase;
+  margin-top: 20px;
+`
+export const InfoWrapper = styled.div`
+  margin-top: 120px;
+`
+export const Info = styled.p`
+  font-family: Helvetica;
+  font-size: 1em;
+  line-height: 1.21;
+  text-align: center;
+  color: #ffffff;
+`
+export const Navigation = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin-top: 100px;
+`
+export const NavLink = styled(Link)`
+  text-decoration: none;
+  font-size: 1em;
+  text-align: center;
+  color: #ffffff;
+  text-transform: uppercase;
+  width: 300px;
+  padding: 1em 0;
+  border-radius: 20px;
+  border: solid 3px #ffffff;
+  margin-bottom: 20px;
+`
