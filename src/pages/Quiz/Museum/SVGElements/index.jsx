06e38068d45d04c58f6b57c8422b62b8f73db50a
Денис Магазейщиков <denismagazeyshchikov@Den.local>
2019-03-31 17:58:43 +0300

    new museum image

--- src/pages/Quiz/Museum/SVGElements/index.jsx
+++ src/pages/Quiz/Museum/SVGElements/index.jsx
@@ -11,14 +11,14 @@ const SVGElements = () => (
         <circle id="Oval-2" fill="none" cx="219" cy="658" r="5" />
         <circle id="Oval-3" fill="none" cx="319" cy="629" r="5" />
         <circle id="Oval-4" fill="none" cx="535" cy="624" r="5" />
-        <circle id="Oval-5" fill="none" cx="635" cy="653" r="5" />
-        <circle id="Oval-6" fill="none" cx="703" cy="682" r="5" />
-        <circle id="Oval-7" fill="none" cx="409" cy="557" r="5" />
-        <circle id="Oval-8" fill="none" cx="443" cy="556" r="5" />
+        <circle id="Oval-5" fill="none" cx="632" cy="653" r="5" />
+        <circle id="Oval-6" fill="none" cx="699" cy="683" r="5" />
+        <circle id="Oval-7" fill="none" cx="404" cy="560" r="5" />
+        <circle id="Oval-8" fill="none" cx="449" cy="559" r="5" />
         <circle id="Oval-9" fill="none" cx="376" cy="454" r="5" />
-        <circle id="Oval-10" fill="none" cx="422" cy="454" r="5" />
+        <circle id="Oval-10" fill="none" cx="423" cy="454" r="5" />
         <circle id="Oval-11" fill="none" cx="469" cy="454" r="5" />
-        <circle id="Oval-12" fill="none" cx="422" cy="293" r="5" />
+        <circle id="Oval-12" fill="none" cx="422" cy="319" r="5" />
       </g>
     </g>
   </SVGContainer>
